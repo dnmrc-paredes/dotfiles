77b6466af62720dd14bddd2324d7fb9cc820ecc9
dnmrcprds <dnmrcprds@gmail.com>
2026-09-01 21:05:30 +0800

    refactor: added keymap for TodoTelescope

--- private_dot_config/nvim/lua/plugins/todo.lua
+++ private_dot_config/nvim/lua/plugins/todo.lua
@@ -6,6 +6,13 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
+        },
+        keys = {
+            {
+                '<leader>st',
+                '<cmd>TodoTelescope keywords=FIX,BUG,ISSUE,TODO,NOTE,TEST<cr>',
+                { desc = 'Search for TODO comments' }
+            }
         }
     }
 }
