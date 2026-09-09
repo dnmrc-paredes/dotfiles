73eb96ce03b3d1ea647a8bc6dd5638750ef6ce27
dnmrcprds <dnmrcprds@gmail.com>
2026-09-10 00:04:43 +0800

    feat: added autotag plugin
    - added neoscroll

--- private_dot_config/nvim/lua/plugins/autocomplete.lua
+++ private_dot_config/nvim/lua/plugins/autocomplete.lua
@@ -10,33 +10,29 @@ return {
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- All presets have the following mappings:
-            -- C-space: Open menu or open docs if already open
-            -- C-n/C-p or Up/Down: Select next/previous item
-            -- C-e: Hide menu
-            -- C-k: Toggle signature help (if signature.enabled = true)
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = 'default',
-            ['<TAB>'] = { 'select_and_accept' },
-            ['<CR>'] = { 'accept', 'fallback' }
-        }
+            -- Keymaps config
+            keymap = { 
+                preset = 'default',
+                ['<TAB>'] = { 'select_and_accept' },
+                ['<CR>'] = { 'accept', 'fallback' }
+            }, -- Properly ends the keymap table, NOT the opts table
+
+            -- Only show the documentation popup when manually triggered
+            completion = { 
+                documentation = { auto_show = false },
+                accept = { auto_brackets = { enabled = true } }
+            },
+
+            -- Explicitly provide sources inside opts
+            sources = { 
+                default = { 'lsp', 'path', 'snippets', 'buffer' } 
+            },
+
+            -- Fuzzy matcher configuration
+            fuzzy = { 
+                implementation = "rust" 
+            },
+        },
     },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    -- (Default) list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"`
-    -- See the fuzzy documentation for more information
-},
 }
+
