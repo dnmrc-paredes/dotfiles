2a5bf94a526f92a5131600506fe2985b7d0e3f8d
dnmrcprds <dnmrcprds@gmail.com>
2026-09-14 12:16:58 +0800

    feat: added supermaven plugin

---
+++ private_dot_config/nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_suggestion = "<C-[>",
+                    clear_suggestion = "<C-]>",
+                    accept_word = "<C-j>",
+                },
+                ignore_filetypes = { cpp = true }, -- or { "cpp", }
+            })
+        end,
+    }
+}
